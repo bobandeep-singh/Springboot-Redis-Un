587c5e222e89f8ea2f5473346c4b7f90d3f65933
Bobandeep Singh <bobandeep_singh@apple.com>
2021-05-29 13:39:32 +0530

    project created from start.spring.io

---
+++ src/main/java/com/redis/learn/SpringbootRedisUnApplication.java
@@ -0,0 +1,13 @@
+package com.redis.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisUnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisUnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/learn/SpringbootRedisUnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisUnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
